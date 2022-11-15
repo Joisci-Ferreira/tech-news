c7eabc8edb47630552a238f14ba4ba905c948903
Joiscilene <joisci.lene@hotmail.com>
2022-11-14 21:02:16 -0300

    adicionando funação scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -39,6 +39,31 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = len(selector.css("comment-list").getall())
+    summary = "".join(
+        selector.css(
+            "div.entry-content > p:nth-of-type(1) *::text"
+        ).getall()
+    ).strip()
+    tags = selector.css("a[rel='tag']::text").getall()
+    category = selector.css("span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category
+    }
 
 
 # Requisito 5
