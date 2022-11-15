ad88246325955cfa8bcb707143a087e3f9192e1c
Joiscilene <joisci.lene@hotmail.com>
2022-11-14 23:22:55 -0300

    adicionando funação search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,27 @@
+from tech_news.database import search_news
+
+
+def newsElements(news_list):
+    formatted_news_list = []
+
+    for news in news_list:
+        formatted_news = (news["title"], news["url"])
+        formatted_news_list.append(formatted_news)
+
+    return formatted_news_list
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news_list = search_news({
+        "title": {
+            "$regex": title,
+            "$options": "i"
+        }
+    })
+
+    return newsElements(news_list)
 
 
 # Requisito 7
